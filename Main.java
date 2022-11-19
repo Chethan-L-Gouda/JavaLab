89a60bf9ebe9820bce172ca5ef20c24414fd85d0
Chethan L <63370113+Chethan-L-Gouda@users.noreply.github.com>
2022-11-19 17:26:18 +0000

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package CS013;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        EmpDet s;
-        s=new EmpDet();
-    }
-}
