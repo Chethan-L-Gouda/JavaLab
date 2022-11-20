1a6c20b83c5ab8e3f90ecba925939497da0e7a06
Chethan L <chethan.shanta@gmail.com>
2022-11-20 12:23:48 +0000

    Add demo of Overloading program

---
+++ DmMCOvrl.java
@@ -0,0 +1,11 @@
+package CS013;
+
+public class DmMCOvrl {
+    DmMCOvrl(){
+
+        MCOvrlding m1 = new MCOvrlding();
+        MCOvrlding m2 = new MCOvrlding("World");
+        m1.add();
+        m1.add(892,929);
+    }
+}
